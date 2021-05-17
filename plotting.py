ace7bc689b6f75e83c5e5afd32c177067e0184d7
Joao Lima <joaoplima@ua.pt>
2021-05-18 00:02:25 +0200

    update plotting

--- plotting.py
+++ plotting.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 def plot_corr(x, y, df):
     """ 
     Returns a barplot using the x and y from the dataframe provided. With this plot we can see the correlations between the x and y
