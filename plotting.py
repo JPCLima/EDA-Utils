7f09c42410634dc893778022311977f19bdd0c58
Joao Lima <joaoplima@ua.pt>
2021-05-18 21:32:50 +0200

    update plotting file

--- plotting.py
+++ plotting.py
@@ -5,6 +5,7 @@ def plot_corr(x, y, df):
     """ 
     Returns a barplot using the x and y from the dataframe provided. With this plot we can see the correlations between the x and y
     """
+    sns.set(font_scale=1.5)
     fig = plt.figure(figsize = (10,6))
-    sns.barplot(x = x, y = y, data = df).set_title(y)
+    sns.barplot(x = x, y = y, data = df)
     plt.show()
